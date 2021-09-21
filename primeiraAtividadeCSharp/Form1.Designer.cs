c4567881c9c556012caa7d32f579d1adeb45b08c
Jonathan Previtalli <jonathan.previtalli@gmail.com>
2021-09-21 08:30:17 -0300

    Add project files.

---
+++ primeiraAtividadeCSharp/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿
+namespace primeiraAtividadeCSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "BOLEIRAS",
+            "PORTA-DOCES",
+            "ROCAMBOLEIRA",
+            "BOLO-FAKE",
+            "VASOS",
+            "PAINEL",
+            "MESA",
+            "TAPETES"});
+            this.comboBox1.Location = new System.Drawing.Point(193, 276);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(289, 24);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(193, 322);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(289, 24);
+            this.comboBox2.TabIndex = 1;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.MediumAquamarine;
+            this.label1.Location = new System.Drawing.Point(8, 280);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Tipo (produto):";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.MediumAquamarine;
+            this.label2.Location = new System.Drawing.Point(8, 326);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Item (produto):";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Location = new System.Drawing.Point(516, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(427, 363);
+            this.panel1.TabIndex = 4;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(21, 24);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(388, 314);
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(470, 235);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(959, 387);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Visualizador de Estoque";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ primeiraAtividadeCSharp/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace primeiraAtividadeCSharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {   //array das minhas imagens
+            string[] nomeImagensDosItens = Directory.GetFiles (@"C:\Users\jonat\OneDrive\Área de Trabalho\ProdutosPP");
+
+            //para cada imagem dentro do array
+            foreach (string img in nomeImagensDosItens)
+            {   
+                // Colocar 2 barras para o separador na barra
+                string[] separador = img.Split('\\');
+                string[] nomeSelecionado = separador[6].Split('.');
+                
+                // testando
+                //MessageBox.Show(nomeSelecionado[0]);
+
+                if(comboBox2.Text == nomeSelecionado[0])
+                {
+                    pictureBox2.ImageLocation = @"C:\Users\jonat\OneDrive\Área de Trabalho\ProdutosPP\" + comboBox2.Text + ".jpeg";
+                    pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+                }
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBox2.Text = string.Empty;
+
+            if(comboBox1.Text == "BOLEIRAS")
+            {
+                comboBox2.Items.Clear();
+                comboBox2.Items.Add("BOLEIRA AMARELA");
+                comboBox2.Items.Add("BOLEIRA VERMELHA");
+            }
+            else if(comboBox1.Text == "PORTA-DOCES")
+            {
+                comboBox2.Items.Clear();
+                comboBox2.Items.Add("PORTA-DOCES MICKEY");
+                comboBox2.Items.Add("PORTA-DOCES PRETA");
+            }
+            else if (comboBox1.Text == "ROCAMBOLEIRA")
+            {
+                comboBox2.Items.Clear();
+                comboBox2.Items.Add("ROCAMBOLEIRA VERMELHA");
+                comboBox2.Items.Add("ROCAMBOLEIRA AMARELA");
+
+            }
+            else if (comboBox1.Text == "BOLO-FAKE")
+            {
+                comboBox2.Items.Clear();
+                comboBox2.Items.Add("BOLO-FAKE BATIZADO");
+                comboBox2.Items.Add("BOLO-FAKE AVENGERS");
+            }
+            else if (comboBox1.Text == "VASOS")
+            {
+                comboBox2.Items.Clear();
+                comboBox2.Items.Add("VASO MADEIRA");
+                comboBox2.Items.Add("VASO AMARELO");
+            }
+            else if (comboBox1.Text == "PAINEL")
+            {
+                comboBox2.Items.Clear();
+                comboBox2.Items.Add("PAINEL MURO INGLES");
+                comboBox2.Items.Add("PAINEL MADEIRA");
+            }
+            else if (comboBox1.Text == "MESA")
+            {
+                comboBox2.Items.Clear();
+                comboBox2.Items.Add("MESA CAVALETE");
+                comboBox2.Items.Add("MESA ARABESCO");
+            }
+            else if (comboBox1.Text == "TAPETES")
+            {
+                comboBox2.Items.Clear();
+                comboBox2.Items.Add("TAPETE PERSA");
+                comboBox2.Items.Add("TAPETE GRAMA SINTETICA");
+            }
+
+        }
+    }
+}
